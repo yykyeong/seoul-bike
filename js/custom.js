17d22aa6f82a49d170abb5c61b144a95b941f2cc
yykyeong <yykyeong@naver.com>
2023-09-08 16:18:29 +0900

    app-section

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+
+
+  /* app-slider */
+  $('.app-slider').slick({
+    infinite: true,
+    arrows: false, 
+    dots: true,
+    autoplay: true, 
+    autoplaySpeed: 2000, 
+  })
+
+})
